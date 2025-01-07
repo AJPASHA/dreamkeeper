9cb3b1653510e790928a2e6fffd247128b060f91
AJPASHA <alexanderjpasha@outlook.com>
2025-01-07 21:00:21 +0000

    implementing Core Navigation

---
+++ lib/pages/index/pages/browser.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+/// The class for the file browser screen
+/// 
+class Browser extends StatelessWidget {
+  const Browser({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      body: Column(
+        children: [
+          Text("Browser Screen coming soon!"),
+          Placeholder(),
+        ],
+      )
+    );
+  }
+}
---
+++ lib/pages/index/pages/tabs.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+/// The class for the tabs screen
+/// 
+class Tabs extends StatelessWidget {
+  const Tabs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      body: Column(
+        children: [
+          Text("Tab Switcher Screen coming soon!"),
+          Placeholder(),
+        ],
+      )
+    );
+  }
+}
